0ece31b6201598c0f67512205a7e7849e0decc6c
Kevin5621 <yohanes123kevin123@gmail.com>
2025-08-01 16:36:52 +0700

    refactor: remove TextSlideUpDemo component from Home page to streamline layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,11 @@ import { HeroSection } from '@/components/sections/HeroSection'
 import { AboutSection } from '@/components/sections/AboutSection'
 import { SelectedWorks } from '@/components/sections/SelectedWorks'
 import { ContactSection } from '@/components/sections/ContactSection'
-import { TextSlideUpDemo } from '@/components/sections/TextSlideUpDemo'
 
 export default function Home() {
   return (
     <div className="min-h-screen hero-page">
       <HeroSection />
-      <TextSlideUpDemo />
       <AboutSection />
       <SelectedWorks />
       <ContactSection />
